030d911eeb6df46924e9e44e22e5a841259bc6a1
FabioSousa89 <fabio.sousa17@gmail.com>
2022-04-24 23:41:50 -0300

    ajuste

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,14 +1,36 @@
 
+import 'dart:convert';
+
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/models/transaction.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
-void findAll()  async {
+Future<List<Transaction>> findAll()  async {
     
     Client client =    InterceptedClient.build(
       interceptors: [LoggingInterceptor()]
     );
 
     final Response response = await client.get(Uri.parse("http://192.168.86.22:8080/transactions"));
+
+    final List<dynamic> decodedJson = jsonDecode(response.body);
+
+    final List<Transaction> transactions = [];
+    for(Map<String, dynamic> e in decodedJson){
+      final Map<String, dynamic> contactJson = e['contact'];
+      final Transaction transaction = Transaction(
+        e['value'], 
+        Contact(
+          0, 
+          contactJson['name'], 
+          contactJson['accountNumber']
+        )
+      );
+      transactions.add(transaction);
+    }
+
+    return transactions;
     
 }
 
@@ -30,6 +52,7 @@ class LoggingInterceptor implements InterceptorContract {
     print('status code ${data.statusCode}');
     print('headers ${data.headers}');
     print('body ${data.body}');   
+
 
     return data;
   }
